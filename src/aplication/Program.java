4c7329fa4d005993f526122ff49fe1790f3d9d2a
Iago Oliveira <iagotcoliveira07@gmail.com>
2024-02-22 19:08:23 -0300

    First class Position

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,13 @@
 package aplication;
 
+import boargame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
-System.out.println("Heloo world!");
+
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 
 }
