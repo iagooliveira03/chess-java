290c32688d4ab97f0fd4f47e33341bd28777003c
iagooliveira03 <iagotcoliveira07@gmial.com>
2024-02-27 11:15:47 -0300

    Little improvement in board printing

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,6 +1,6 @@
 package aplication;
 
-//teste3
+
 import chess.ChessMatch;
 
 public class Program {
@@ -8,9 +8,9 @@ public class Program {
 	public static void main(String[] args) {
 
 		ChessMatch chessMatch =  new ChessMatch();
-		UI.printBoard(chessMatch.getpieces());
+		UI.printBoard(chessMatch.getPieces());
 		
-		//teste
+	
 	}
 
 }
